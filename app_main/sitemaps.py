4e11eb0e58073197dca8c5f72f411787dff40029
Desead <news-piter@mail.ru>
2025-09-12 22:58:48 +0300

    делаем сайт мультиязычным

--- app_main/sitemaps.py
+++ app_main/sitemaps.py
@@ -3,7 +3,9 @@ from django.urls import reverse
 
 class StaticViewSitemap(Sitemap):
     changefreq = "daily"
-    priority = 1.0
+    priority = 1
+    i18n = True        # генерирует URL для всех LANGUAGES
+    # alternates = True  # добавит <xhtml:link rel="alternate" ...>
 
     def items(self):
         # укажи имена URL, которые должны попасть в sitemap
