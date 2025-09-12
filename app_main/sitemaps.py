55046542b7a7652f0d4773f04ef12c6872810f6d
Desead <news-piter@mail.ru>
2025-09-13 02:23:36 +0300

    делаем сайт мультиязычным

--- app_main/sitemaps.py
+++ app_main/sitemaps.py
@@ -5,7 +5,6 @@ class StaticViewSitemap(Sitemap):
     changefreq = "daily"
     priority = 1
     i18n = True
-    alternates = True  # важно для <xhtml:link rel="alternate" hreflang="...">
 
     def items(self):
         return ["home"]
